13e07a1ff9e7dfe5487781f33b9cd61d45b2b38b
AAASmokey <https://github.com/AAASmokey/AAASmokey.git>
2025-03-31 20:33:27 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+
+</script>
+
+<template>
+<div class="todo">
+  <div class="title">Todo app</div>
+
+  <div class="todo-form">
+    <input type="text" class="todo-input">
+    <div class="todo-button"></div>
+  </div>
+</div>
+</template>
+
+<style>
+
+</style>
